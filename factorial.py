cf37943124a2ddade71d56c98fb63df2f666d90e
lalit-jamdangnee <110276457+lalit-jamdangnee@users.noreply.github.com>
2022-07-30 11:26:18 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,4 +1,7 @@
 a=int(input("Enter a number"))
 def fact(n):
-  return n*fact(n-1)
+  if n==1 or n==0:
+    return 1
+  else:
+    return n*fact(n-1)
 print("The factorial of the number is ",fact(a))
